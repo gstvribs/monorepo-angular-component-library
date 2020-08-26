0ff26368390b89f90ae4c0b3ec02a3fcb1f7bd6e
Gustavo Ribeiro <gu_ribeiro@live.com>
2020-08-25 21:26:45 -0300

    chore: including chromatic and bump storybook to version 6

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   tabWidth: 2,
   useTabs: false,
   semi: true,
-  trailingComma: "all",
+  trailingComma: 'all',
   singleQuote: true,
-  endOfLine: "lf"
+  endOfLine: 'auto',
 };
